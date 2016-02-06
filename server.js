bfe74426479e1752e523404cd6d4ff78b117a1e1
Marcode777 <marc102202@hotmail.com>
2016-02-06 10:25:51 -0500

    structure of assignment

---
+++ server.js
@@ -0,0 +1,18 @@
+//look up the code for this exercise on github 
+
+//Create a 3 page website
+// Home
+// Register
+// Dashboard
+// Include jQuery (not from CDN)
+// Include Bootstrap (not from CDN)
+// Create a navbar
+// In the navbar, have a login link. Clicking login should open a bootstrap modal that has an email and password field (and button)
+// Make this look good!
+// On the Registration form, use JavaScript / jQuery to ensure all fields are filled out before POSTing the form
+
+var express = require ('express');
+var app = express ();
+var PORT = 8090;
+
+app.use("/js", express.static('public')
